30b764e3a1fef34dfc406a1c80b456b8ba1c4121
tani-cat <tani.tvet2749@gmail.com>
2023-02-23 17:54:05 +0900

    Initial commit

---
+++ docs/CONTRIBUTING.md
@@ -0,0 +1,3 @@
+# Contributing Guideline
+
+Our project does not need any contributors other than the owner at now.
---
+++ hongo-lunch/src/App.js
@@ -0,0 +1,7 @@
+import MainScreen from "./components/main";
+
+export default function App() {
+  return (
+    <MainScreen />
+  );
+}
