cff420b6e00ad12390e9398763207dfcd4605253
tani-cat <tani.tvet2749@gmail.com>
2023-03-04 20:32:52 +0900

    update list of place

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 import { PageHome } from './components/page-home';
 import { PageResult } from './components/page-result';
 
+const Title = React.memo(() => {
+  return <h2 className="mb-3">本郷ランチ</h2>;
+})
+
 export const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Container className="p-4 text-center">
-        <h2 className="mb-3">本郷ランチ</h2>
+      <Container className="p-4 col-md-8 col-lg-6 text-center">
+        <Title />
         <Routes>
           <Route path="/" element={<PageHome />} />
           <Route path="/result" element={<PageResult />} />
--- src/components/page-result.js
+++ src/components/page-result.js
@@ -1,12 +1,14 @@
 import { Card, Button, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Place from "../data/place.json";
 
 
 export const PageResult = () => {
+  const navigate = useNavigate();
+  // 乱数で決定する
   const resultId = Math.floor(Math.random() * Place.place.length);
-  const result = Place.place[resultId].name;
+  const result = Place.place[resultId];
 
   return (
     <>
@@ -14,20 +16,20 @@ export const PageResult = () => {
         <Card.Body>
           <Card.Title>本日のランチ結果</Card.Title>
           <Card.Text>
-            {result}
+            {result.display_name}
           </Card.Text>
           <Row className="row-cols-auto justify-content-center">
             <Col>
-              <Button as="a" type="button" variant="outline-info" className="me-1" href={`https://twitter.com/intent/tweet?text=今日のランチは「${result}」にします&hashtags=本郷ランチ`} target="_blank" rel="nofollow">Tweet</Button>
+              <Button as="a" type="button" variant="outline-info" className="me-1" href={`https://twitter.com/intent/tweet?text=今日のランチは「${result.display_name}」にします&hashtags=本郷ランチ`} target="_blank" rel="nofollow">Tweet</Button>
             </Col>
             <Col>
-              <Button as="a" type="button" variant="outline-success" href={`https://www.google.com/maps/search/?api=1&query=${result}`} target="_blank" rel="nofollow">Google Map</Button>
+              <Button as="a" type="button" variant="outline-success" href={`https://www.google.com/maps/search/?api=1&query=${result.search_name}`} target="_blank" rel="nofollow">Google Map</Button>
             </Col>
           </Row>
         </Card.Body>
       </Card>
       <br />
-      <Link to="/">もう一度やり直す</Link>
+      <Button variant="secondary" onClick={() => navigate("/")}>もう一度選ぶ</Button>
     </>
   )
 }
